356d15e80c691a772347eef0e3800a912c11781f
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-08-19 16:31:54 +0530

    check some APIs with passing token

--- src/app/dashboard/(router)/user/page.tsx
+++ src/app/dashboard/(router)/user/page.tsx
@@ -1,4 +1,11 @@
-export default function User(){
+import {getAllUsers} from "@/lib/actions";
+
+export default async function User(){
+
+    const allUsers = await getAllUsers()
+
+    console.log(allUsers);
+
     return(
         <div>
             User Manage Dashboard
